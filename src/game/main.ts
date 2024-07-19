855074c6087c0c0242a823ffbd13abeef4aecd40
Nguyen Duc Toan <125717928+duwcston@users.noreply.github.com>
2024-07-19 17:02:55 +0700

    Create Project Template

---
+++ src/game/main.ts
@@ -0,0 +1,28 @@
+import { Boot } from './scenes/Boot';
+import { Game as MainGame} from './scenes/Game';
+import { AUTO, Game } from 'phaser';
+import { Preloader } from './scenes/Preloader';
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config: Phaser.Types.Core.GameConfig = {
+    type: AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scene: [
+        Boot,
+        Preloader,
+        MainGame,
+
+    ]
+};
+
+const StartGame = (parent: string) => {
+
+    return new Game({ ...config, parent });
+
+}
+
+export default StartGame;
